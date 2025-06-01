7e9e515288ad7352ad15789c7470974c6c2c5be9
Massimo07 <150889766+Massimo07@users.noreply.github.com>
2025-06-01 03:02:07 +0200

    app.py

--- app.py
+++ app.py
@@ -49,4 +49,7 @@ def webhook():
     return "!", 200
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    import os
+port = int(os.environ.get('PORT', 10000))
+app.run(host='0.0.0.0', port=port)
+
